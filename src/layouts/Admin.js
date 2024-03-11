e29473a26fdcf8f266d579c2a9a9a32e61c59b99
mukthiyarglobal <145452704+mukthiyarglobal@users.noreply.github.com>
2024-03-11 13:13:01 +0530

    Update Admin.js

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -96,7 +96,7 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={" DevOps TEAM  "}
+        logoText={" Azure TEAM  "}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
